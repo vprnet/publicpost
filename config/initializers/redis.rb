55bb4fc21ba8b15c1bfd6dafb45810f3eb271084
Sean Behan <inbox@seanbehan.com>
2014-06-12 20:51:29 -0400

    override redis to go url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-uri = URI.parse("redis://root@23.253.237.4:6379")
+ENV["REDISTOGO_URL"] = "redis://root@23.253.237.4:6379"
+uri = URI.parse(ENV["REDISTOGO_URL"])
+# uri = URI.parse("redis://root@23.253.237.4:6379")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 REDIS_POOL  = ConnectionPool.new(:size => 8, :timeout => 60) { REDIS }
