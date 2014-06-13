0bc3f00e5491f86c05db77a2d9bfefcec0e44298
Sean Behan <inbox@seanbehan.com>
2014-06-12 20:00:19 -0400

    using redis cloud free tier

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISCLOUD_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 REDIS_POOL  = ConnectionPool.new(:size => 8, :timeout => 60) { REDIS }
