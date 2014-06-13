03bb51ccc87d95910e7a9cbd3ae5e523cd82b62e
Sean Behan <inbox@seanbehan.com>
2014-06-13 12:26:06 -0400

    mail gun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ HeSaidSheSaid::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'he-said-she-said.heroku.com',
+    :domain         => 'publicpost.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
