54434778c662af6166ba37610d40f01e4b4b1dd8
Sean Behan <inbox@seanbehan.com>
2014-06-20 11:26:57 -0400

    setting default from in smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ HeSaidSheSaid::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :from           => ENV["MAIL_DEFAULT_FROM"],
     :domain         => 'publicpost.heroku.com',
     :authentication => :plain,
   }
